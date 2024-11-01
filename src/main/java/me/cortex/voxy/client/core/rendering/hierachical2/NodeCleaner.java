70dff8435dd09ae75ca5d86a4a67c5f4adbe69f0
mcrcortex <nedogimoff@gitbebra.ru>
2024-11-01 17:28:30 +1000

    Funny very incorrect tinkering of culling

--- src/main/java/me/cortex/voxy/client/core/rendering/hierachical2/NodeCleaner.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/hierachical2/NodeCleaner.java
@@ -6,6 +6,9 @@ package me.cortex.voxy.client.core.rendering.hierachical2;
 public class NodeCleaner {
 
 
+    //Clears memory via invoking node manager, clear section or delete node
+    // these should take hint parameters on if when removing section data, to store it in section cache or leave
+
     public void setNodeMemoryUsage() {
         //Needs to bubble up information to all parents
         // also needs to update a tick/last seen time for node removal
