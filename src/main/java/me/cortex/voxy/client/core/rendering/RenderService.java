ba43036448320de6503a598f673136e4b1466cd4
mcrcortex <nedogimoff@gitbebra.ru>
2025-01-05 08:40:20 +1000

    Change loading time

--- src/main/java/me/cortex/voxy/client/core/rendering/RenderService.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/RenderService.java
@@ -48,7 +48,7 @@ public class RenderService<T extends AbstractSectionRenderer<J, ?>, J extends Vi
 
         //Max sections: ~500k
         //Max geometry: 1 gb
-        this.sectionRenderer = (T) createSectionRenderer(this.modelService.getStore(),1<<20, (1L<<31)-1024);
+        this.sectionRenderer = (T) createSectionRenderer(this.modelService.getStore(),1<<20, (1L<<32)-1024);
 
         //Do something incredibly hacky, we dont need to keep the reference to this around, so just connect and discard
         var router = new SectionUpdateRouter();
@@ -144,9 +144,9 @@ public class RenderService<T extends AbstractSectionRenderer<J, ?>, J extends Vi
         //this.nodeManager.insertTopLevelNode(WorldEngine.getWorldSectionId(4, 0,0,0));
     }
 
-    private int q = -1024;
+    private int q = -128;
     public void setup(Camera camera) {
-        if (q<(151*151*2)&&q++>=0) {
+        for (int i = 0; i<32 && q<(151*151*2)&&q++>=0;i++) {
             this.nodeManager.insertTopLevelNode(WorldEngine.getWorldSectionId(4, (q%151)-75, ((q/151)/151)-1, ((q/151)%151)-75));
         }
         this.modelService.tick();
