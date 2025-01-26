05b1fc5ae2832eba188d10cd7a813cd826cd6e18
mcrcortex <nedogimoff@gitbebra.ru>
2025-01-26 20:00:51 +1000

    Remove unused import

--- src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinThreadExecutor.java
+++ src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinThreadExecutor.java
@@ -1,7 +1,6 @@
 package me.cortex.voxy.client.mixin.minecraft;
 
 import me.cortex.voxy.client.LoadException;
-import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
 import net.minecraft.util.thread.ThreadExecutor;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
