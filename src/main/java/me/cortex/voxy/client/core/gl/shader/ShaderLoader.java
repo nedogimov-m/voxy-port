f3666cb51106bfefd9e79eea0c1b6f27c63aec22
mcrcortex <nedogimoff@gitbebra.ru>
2025-09-22 10:54:16 +1000

    slight prep for sodium 0.7

--- src/main/java/me/cortex/voxy/client/core/gl/shader/ShaderLoader.java
+++ src/main/java/me/cortex/voxy/client/core/gl/shader/ShaderLoader.java
@@ -6,7 +6,7 @@ import net.caffeinemc.mods.sodium.client.gl.shader.ShaderParser;
 
 public class ShaderLoader {
     public static String parse(String id) {
-        return ShaderParser.parseShader("#import <" + id + ">", ShaderConstants.builder().build());
+        return "#version 460 core\n"+ShaderParser.parseShader("#import <" + id + ">\n//beans", ShaderConstants.builder().build()).replaceFirst("\n#version .+\n", "\n");
         //return me.jellysquid.mods.sodium.client.gl.shader.ShaderLoader.getShaderSource(new Identifier(id));
     }
 }
