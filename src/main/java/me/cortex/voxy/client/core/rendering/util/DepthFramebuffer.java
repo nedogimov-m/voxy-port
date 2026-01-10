a976d51fee69c3ee68dfaa155c8189368cac9e1c
mcrcortex <nedogimoff@gitbebra.ru>
2026-01-10 13:13:45 +1000

    revert ssao

--- src/main/java/me/cortex/voxy/client/core/rendering/util/DepthFramebuffer.java
+++ src/main/java/me/cortex/voxy/client/core/rendering/util/DepthFramebuffer.java
@@ -32,8 +32,8 @@ public class DepthFramebuffer {
                 this.depthBuffer.free();
             }
             this.depthBuffer = new GlTexture().store(this.depthType, 1, width, height);
-            glTextureParameteri(this.depthBuffer.id, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-            glTextureParameteri(this.depthBuffer.id, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            //glTextureParameteri(this.depthBuffer.id, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            //glTextureParameteri(this.depthBuffer.id, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             this.framebuffer.bind(this.getDepthAttachmentType(), this.depthBuffer).verify();
             return true;
         }
