8ed3efeb0097be21b35afc09f405a0ce6450713b
mcrcortex <nedogimoff@gitbebra.ru>
2026-02-07 14:29:58 +1000

    remove all line defs

--- src/main/java/me/cortex/voxy/client/core/gl/shader/ShaderLoader.java
+++ src/main/java/me/cortex/voxy/client/core/gl/shader/ShaderLoader.java
@@ -6,7 +6,8 @@ import net.caffeinemc.mods.sodium.client.gl.shader.ShaderParser;
 
 public class ShaderLoader {
     public static String parse(String id) {
-        return "#version 460 core\n"+ShaderParser.parseShader("\n#import <" + id + ">\n//beans", ShaderConstants.builder().build()).src().replaceAll("\r\n", "\n").replaceFirst("\n#version .+\n", "\n");
+        var src =  "#version 460 core\n"+ShaderParser.parseShader("\n#import <" + id + ">\n//beans", ShaderConstants.builder().build()).src().replaceAll("\r\n", "\n").replaceFirst("\n#version .+\n", "\n");
+        return src.replaceAll("\n#line [0-9]+ [0-9]+\n", "");
         //return me.jellysquid.mods.sodium.client.gl.shader.ShaderLoader.getShaderSource(new Identifier(id));
     }
 }
